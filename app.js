df8b9a6d386ecfc7c64dd532090a23af840d6416
Pepe Bausá <46072321+PepeBausa@users.noreply.github.com>
2021-02-08 16:38:01 +0100

    http

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const http = require('http');
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.end('Hola Mundo');
+});
+server.listen(80);
 console.log('Hola Mundo');
