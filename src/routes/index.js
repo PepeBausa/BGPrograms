91a50f912815d89941dd5806466c35b2e7b48357
Pepe Bausá <46072321+PepeBausa@users.noreply.github.com>
2021-02-20 16:46:12 +0100

    fin

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,8 +6,8 @@ router.get('/', (req,res) => {
     res.render('index.html' , {title: 'BGPrograms'});
 });
 
-router.get('/bi', (req,res) => {
-    res.render('bi.html' , {title: 'Balneario Illetas'});
+router.get('/balneario', (req,res) => {
+    res.render('balneario.html' , {title: 'Balneario Illetas'});
 });
 
 module.exports =  router;
