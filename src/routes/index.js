d8e1038f154f9cf535b70e19db893612e2e3447a
Pepe Bausá <46072321+PepeBausa@users.noreply.github.com>
2021-02-23 13:49:30 +0100

    balneario

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,23 @@ router.get('/', (req,res) => {
 });
 
 router.get('/balneario', (req,res) => {
-    res.render('balnearioilletas.html' , {title: 'Balneario Illetas'});
+    res.render('balneario/balnearioilletas.html' , {title: 'Balneario Illetas'});
+});
+
+router.get('/balneario/albaranes', (req,res) => {
+    res.render('balneario/albaranes.html' , {title: 'Balneario Illetas - Albaranes'});
+});
+
+router.get('/balneario/productos', (req,res) => {
+    res.render('balneario/productos.html' , {title: 'Balneario Illetas - Productos'});
+});
+
+router.get('/balneario/proveedores', (req,res) => {
+    res.render('balneario/proveedores.html' , {title: 'Balneario Illetas - Proveedores'});
+});
+
+router.get('/balneario/escandallos', (req,res) => {
+    res.render('balneario/escandallos.html' , {title: 'Balneario Illetas - Escandallos'});
 });
 
 module.exports =  router;
